b69946c1167efc6ab144326f60b3592302082491
Louie <94155642+woojinchoi17@users.noreply.github.com>
2023-06-18 17:16:33 +0900

    Netflix Main Scene Clone using UICollectionView

---
+++ NetflixMain/NetflixMain/ContentCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ContentCollectionViewCell.swift
+//  NetflixMain
+//
+//  Created by woojin Choi on 2023/06/18.
+//
+
+import UIKit
+import SnapKit
+
+class ContentCollectionViewCell: UICollectionViewCell {
+    let imageView = UIImageView()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 5
+        contentView.clipsToBounds = true
+        
+        imageView.contentMode = .scaleToFill
+        
+        contentView.addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
