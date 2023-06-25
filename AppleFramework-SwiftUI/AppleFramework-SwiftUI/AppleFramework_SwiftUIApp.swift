886fc1a80effdd288351df30faf8f040ab02ed49
Louie <94155642+woojinchoi17@users.noreply.github.com>
2023-06-25 14:14:23 +0900

    애플프레임워크 소개앱 SwiftUI로 만들기

---
+++ AppleFramework-SwiftUI/AppleFramework-SwiftUI/AppleFramework_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFramework_SwiftUIApp.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by woojin Choi on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFramework_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkListView()
+        }
+    }
+}
