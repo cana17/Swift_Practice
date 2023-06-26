b83e5d5ef9e92433aa5bbc02062e11b9d535b149
Louie <94155642+woojinchoi17@users.noreply.github.com>
2023-06-26 19:48:28 +0900

    ObservedObject 활용

---
+++ StockRank-SwiftUI-Observe/StockRank-SwiftUI/StockRankView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  StockRank-SwiftUI
+//
+//  Created by woojin Choi on 2023/06/26.
+//
+
+import SwiftUI
+
+struct StockRankView: View {
+    
+//    @State var list = StockModel.list
+    @StateObject var vm = StockRankViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List($vm.models) { $item in
+                ZStack {
+                    NavigationLink {
+                        StockDetailView(viewModel: vm, stock: $item)
+                    } label: {
+                        EmptyView()
+                    }
+                    StockRankRow(stock: $item)
+                }
+                .listRowInsets(EdgeInsets())
+                .frame(height: 80)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Stock Rank")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankView()
+            .preferredColorScheme(.dark)
+    }
+}
