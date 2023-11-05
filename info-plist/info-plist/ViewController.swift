4f1afaf0967d2dac70e0cee0d169c5af81ff77a1
cana17 <94155642+cana17@users.noreply.github.com>
2023-11-05 09:09:22 +0900

    info.plist 간단 실습해보기
    토트넘 우승 가즈아

---
+++ info-plist/info-plist/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  info-plist
+//
+//  Created by woojin Choi on 11/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        readPropertyList()
+    }
+
+    private func readPropertyList() {
+        guard let path = Bundle.main.path(forResource: "tutorial-list", ofType: "plist") else {
+            print("Path for resource is incorrect")
+            return
+        }
+        
+        guard let data = FileManager.default.contents(atPath: path) else {
+            print("Data conversion failed")
+            return
+        }
+        
+        do {
+            let propertyList = try PropertyListSerialization.propertyList(from: data, format: nil)
+            guard let dict = propertyList as? [String: Any] else {
+                print("Dictionary casting failed")
+                return
+            }
+            
+            // Root
+            if let value = dict["number"] as? Int {
+                print("Number: \(value)")
+            }
+            
+            // Dictionary
+            if let dictionary = dict["dictionary-items"] as? [String: Any] {
+                if let item = dictionary["X1"] as? String {
+                    print("Dictionary item: \(item)")
+                }
+            }
+            
+            // Array
+            if let array = dict["array-items"] as? [Any] {
+                if let item = array[0] as? String {
+                    print("Array item: \(item)")
+                }
+            }
+        } catch let error {
+            print("Error serializing plist: \(error.localizedDescription)")
+        }
+    }
+}
